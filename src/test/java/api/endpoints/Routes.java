246fae3f76eba58c182ab89b508cbc66ed065b8f
Abhishek <Abhi12062000@gmail.com>
2026-05-15 18:15:05 +0530

    adding new commit now

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -2,10 +2,20 @@ package api.endpoints;
 
 public class Routes {
 
-	public static String baseUrl = "https://petstore.swagger.io/v2";
+	
 
+	public static String baseUrl = "https://petstore.swagger.io/v2";
+	public static String baseUrlInstrument = "http://localhost:8080";
+	
+
+	public static String auth = baseUrlInstrument+ "/api/auth/login";
+	public static String addInstrument =baseUrlInstrument+ "/api/instruments";
+	public static String getInstrument = baseUrlInstrument+"/api/instruments/{id}";
 	public static String getUrl = baseUrl + "/user/{username}";
 	public static String postUrl = baseUrl + "/user";
 	public static String updateUrl = baseUrl + "/user/{username}";
 	public static String deleteUrl = baseUrl + "/user/{username}";
+	public static String postImage = baseUrl + "/pet/{petId}/uploadImage";
+	public static String addPetPost = baseUrl + "/pet";
+	public static String addedPetPost = baseUrl + "/pet/set";
 }
